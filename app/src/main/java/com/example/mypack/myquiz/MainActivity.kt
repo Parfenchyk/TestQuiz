c7fc028260ed9cc407df2460c6a72c8c8f7179ed
Parfenchyk <petrovich.minsk@gmail.com>
2023-07-28 21:57:20 +0300

    v1

--- app/src/main/java/com/example/mypack/myquiz/MainActivity.kt
+++ app/src/main/java/com/example/mypack/myquiz/MainActivity.kt
@@ -1,16 +1,86 @@
 package com.example.mypack.myquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var quizItemList: List<QuizItem>
+    private lateinit var startButton: Button
+    private var activeQuiz: MutableMap<QuizItem, AnswerType>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        quizItemList = createQuestionList()
+        startButton = findViewById(R.id.btn_quiz_start)
+        startButton.setOnClickListener {
+            if (activeQuiz == null) {
+                startNewQuiz()
+            } else {
+                TODO()
+            }
+            var currentQuizItem: QuizItem? = null
+            activeQuiz!!.forEach { (key, value) ->
+                if (value == AnswerType.UNDEFINED) {
+                    currentQuizItem = key
+                }
+                val newIntent = Intent(applicationContext, QuestionActivity::class.java)
+                newIntent.putExtra("question", currentQuizItem!!.question)//TODO
+                newIntent.putExtra("answer0", currentQuizItem!!.answer.get(0))
+                newIntent.putExtra("answer1", currentQuizItem!!.answer.get(1))
+                newIntent.putExtra("correctIndex", currentQuizItem!!.correctId)
+                startActivity(newIntent)
+            }
+        }
+
+
     }
-}
 
-data class QuizItem(val question: String, val answer: List<String>, val correctId: Int) {
+    private fun startNewQuiz() {
+        activeQuiz = mutableMapOf()
+        for (index in 0..3) {
+            activeQuiz!!.put(quizItemList.get(index), AnswerType.UNDEFINED)
+        }
+    }
 
+    data class QuizItem(
+        val question: String = "how are you?",
+        val answer: List<String>,
+        val correctId: Int
+    )
+//data class QuizItem(val question: String, val answer: List<String>, val correctId: Int)
+
+    fun createQuestionList(): List<QuizItem> {
+        val quizItemList: MutableList<QuizItem> = mutableListOf()
+        quizItemList.add(QuizItem("Котлин статический язык?", listOf("yes", "no"), 0))
+        quizItemList.add(
+            QuizItem(
+                question = "Сколько примитивных типов в Котлине?",
+                listOf("8", "0"),
+                correctId = 1
+            )
+        )
+        quizItemList.add(
+            QuizItem(
+                "Является ли Котлин функциональным языком?",
+                listOf("yes", "no"),
+                1
+            )
+        )
+        quizItemList.add(
+            QuizItem(
+                "Чем являются функции в Котлин?",
+                listOf("object", "variable"),
+                0
+            )
+        )
+        return quizItemList
+    }
+
+    enum class AnswerType {
+        CORRECT, INCORRECT, UNDEFINED
+    }
 }
 
